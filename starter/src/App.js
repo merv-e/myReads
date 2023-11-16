a396519fca7cc5e00b92871987259dc6db7a662c
merv-e <34655454+merv-e@users.noreply.github.com>
2023-11-16 19:29:07 +0100

    - loading class is added. - some unnecessary notes are deleted.

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import * as BooksAPI from "./BooksAPI";
+import * as BooksAPI from "./services/BooksAPI";
 import SearchBooks from "./components/Search/SearchBooks";
 import { Route, Routes } from "react-router-dom";
 import Library from "./components/Library/Library";
@@ -8,40 +8,25 @@ import Library from "./components/Library/Library";
 const App = () => {
   // data comes with the API call will be stored here for the main page
   const [data, setData] = useState([]);
+
   const getData = async () => {
     const res = await BooksAPI.getAll();
     setData(res);
-    /* YUKARIDAKINI SILIP SUNU BI DENEYELIM */
-    //  .then(books => setData(books));
   };
 
   useEffect(() => {
-    // eger bookshelf managerdaki veri degisiyorsa... getData'yi cagir.
-    //  if(load)
     getData();
-  }, [data]); //
-  // not: contacts.app'de contacts'i dependency arr. yazilmamis
-
-  // console.log(data);
-
-  //data'yi dependency arr eklemezsek surekli render etmiyor. ancak cikarirsak da bu sefer kitap rafını değiştirdiğimizde anlık işlem yapmıyor ! :/
-
-  // book and shelf information will be transfered from child components which are BookShelf and SearchBooks.
+  }, []);
 
   const updateBook = (book, shelf) => {
     BooksAPI.update(book, shelf);
-    // getData(); // bu nereden cikti islevsel mi hicbir fikrim yok :/
+    getData();
   };
 
-  //  bookId and shelf information will be transfered from child components.
   const getBook = (bookId, shelf) => {
     const res = BooksAPI.get(bookId);
     updateBook(res, shelf);
   };
-
-  // this call is made (getBook), so that after user searchs a book and is chosen to be on any shelf, it'll also update the book in backend.
-
-  // That's what i Thought it would do (above), however it perfectly works without this API call. I'm not entirely sure what I'm supposed to use this for.
 
   return (
     <div className="app">
@@ -69,7 +54,3 @@ const App = () => {
 };
 
 export default App;
-
-// get Book async ve await silince console.log'da aldığım hataları artık almıyorum.
-
-//getBook hicbir yerde cagrilmiyor.
