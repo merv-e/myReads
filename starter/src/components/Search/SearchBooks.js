8fa498049a3a3572cddaedadb6722e3abaf8c3bd
merv-e <34655454+merv-e@users.noreply.github.com>
2023-11-27 12:39:21 +0100

    - comments are deleted - some minor changes are made in the logic of the app - error state is styled

--- starter/src/components/Search/SearchBooks.js
+++ starter/src/components/Search/SearchBooks.js
@@ -5,59 +5,50 @@ import ShelfInfo from "./ShelfInfo";
 import { PropTypes } from "prop-types";
 
 const SearchBooks = ({ books, updateBook, getBook }) => {
-  // data comes with the API call will be stored in the variable below.
   const [newSetOfBooks, setNewSetOfBooks] = useState([]);
 
   const [query, setQuery] = useState("");
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [isNothingFound, setIsNothingFound] = useState(false);
 
-  const [isSearching, setIsSearching] = useState(false);
-
-
-  // searchBook will take the query variable (a.k.a where user searchs for books) , if it's empty which is the default value, it'll show nothing. However, if user starts typing something the query will be updated and searchResults variable will filter it accordingly.
   const searchResults =
     query === ""
-      ? newSetOfBooks
-      : newSetOfBooks.filter((book) =>
-          book.title.toLowerCase().includes(query.toLowerCase())
+      ? []
+      : newSetOfBooks.filter(
+          (book) => book.title.toLowerCase().includes(query.toLowerCase())
+          // ||
         );
 
   useEffect(() => {
     // when the user navigates to the search page and types a title, id etc. it'll show the relevant books.
     const searchBooks = async () => {
-      setIsSearching(true);
       try {
         setIsLoading(true);
-
         const response = await BooksAPI.search(query, 20);
 
-        console.log("response", response);
-        console.log("response:", response.error);
-
-        if (!response.error) {
+        if (response.error !== "empty query") {
+          setIsNothingFound(false);
           setNewSetOfBooks(response);
           setError(null);
-        }
+        } else setIsNothingFound(true);
+
       } catch (error) {
         setError("An error occurred while fetching books.");
         console.error("Error fetching books:", error);
       }
       setIsLoading(false);
-      setIsSearching(false);
     };
 
     const debounce = setTimeout(() => {
       if (query) {
         searchBooks();
       }
-    }, 3000); // cleanup function
+    }, 1000);
 
+    // cleanup function
     return () => clearTimeout(debounce);
   }, [query]);
-
-  // console.log("newSetOfBooks", newSetOfBooks);
-  // console.log("searchResults", searchResults);
 
   return (
     <div className="search-books">
@@ -74,16 +65,13 @@ const SearchBooks = ({ books, updateBook, getBook }) => {
           />
         </div>
       </div>
+
       {isLoading && <p className="loading">Loading...</p>}
-
-
-      {/* searchResults.length === 0 */}
-      {!isLoading && query !== "" && searchResults.length === 0 && (
-        <p className="no-book-found">No books found for this query.</p>
+      {isNothingFound && (
+        <p className="no-book-found">No book is found for this query.</p>
       )}
-
-
       {!isLoading && error && <p className="error">{error}</p>}
+
       {!isLoading && (
         <div className="search-books-results">
           <ShelfInfo
