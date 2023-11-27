ff1e9875e57c895492c7a997cc868f80c8b97a2d
merv-e <34655454+merv-e@users.noreply.github.com>
2023-11-27 11:54:31 +0100

    path of svg files are changed for svg files according to the latest changes and some state updates for error etc. are added with basic styling - name of the component is changed. - a better debouncing function is added.

--- starter/src/components/Book.js
+++ starter/src/components/Book.js
@@ -5,39 +5,37 @@ const Book = ({
   id,
   title,
   shelf,
-  updateBook,
   authors,
   url,
   book,
+  updateBook,
   getBook,
 }) => {
   return (
-    <>
-      <li key={id}>
-        <div className="book">
-          <div className="book-top">
-            <div
-              className="book-cover"
-              style={{
-                width: 128,
-                height: 193,
-                backgroundImage: `url(${url})`,
-              }}
-            ></div>
-            <BookShelfManager
-              updateBook={updateBook}
-              shelf={shelf}
-              title={book.title}
-              book={book}
-              id={id}
-              getBook={getBook}
-            />
-          </div>
-          <div className="book-title">{title}</div>
-          <div className="book-authors">{authors && authors.join(", ")}</div>
+    <li key={id}>
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${url})`,
+            }}
+          ></div>
+          <BookShelfManager
+            id={id}
+            shelf={shelf}
+            title={book.title}
+            book={book}
+            updateBook={updateBook}
+            getBook={getBook}
+          />
         </div>
-      </li>
-    </>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{authors && authors.join(", ")}</div>
+      </div>
+    </li>
   );
 };
 
